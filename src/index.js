0d2396fa810b1f50bf0dce7ac31ed7dad9019b29
anatol-codifi <70380338+anatol-codifi@users.noreply.github.com>
2021-04-14 17:15:36 +0300

    summary 4 description 4

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "./styles.css";
 
 document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla 333556677!</h1>
+<h1>Hello Vanilla 33355667788!</h1>
 <div>
   We use the same configuration as Parcel to bundle this sandbox, you can find more
   info about Parcel 
